0aa2b468df5db92bf86fd7267832c3d3f0d00130
Riccardo-Grasso <riccardo.grasso@outlook.it>
2021-11-16 18:14:08 +0100

    V_3.0

--- src/components/AlbumsContainer.vue
+++ src/components/AlbumsContainer.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="pt-5">
-    <SearchBar></SearchBar>
+    <FiltroGeneri
+      :categoryList="categoryList"
+      @badgeClick="clickGeneri"
+    ></FiltroGeneri>
     <div class="row row-cols-5 gy-4 gx-5 pt-5">
-      <div class="col" v-for="album in albumList" :key="album.id">
+      <div class="col" v-for="album in filteredAlbumList" :key="album.id">
         <AlbumCard
           :poster="album.poster"
           :title="album.title"
@@ -20,16 +23,29 @@
 import axios from "axios";
 import AlbumCard from "@/components/AlbumCard.vue";
 import Loader from "@/components/Loader.vue";
-import SearchBar from "@/components/SearchBar.vue";
+import FiltroGeneri from "@/components/FiltroGeneri.vue";
 
 export default {
   name: "AlbumsContainer",
-  components: { AlbumCard, Loader, SearchBar },
+  components: { AlbumCard, Loader, FiltroGeneri },
   data() {
     return {
       albumList: [],
+      categoryList: [],
       loading: true,
+      categoryFilter: "",
     };
+  },
+  computed: {
+    filteredAlbumList() {
+      if (!this.categoryFilter) {
+        return this.albumList;
+      }
+
+      return this.albumList.filter((album) => {
+        return this.categoryFilter === album.genre;
+      });
+    },
   },
   mounted() {
     axios
@@ -37,10 +53,21 @@ export default {
       .then((resp) => {
         console.log(resp.data.response);
         this.albumList = resp.data.response;
+        this.albumList.forEach((elemento) => {
+          if (!this.categoryList.includes(elemento.genre)) {
+            this.categoryList.push(elemento.genre);
+          }
+        });
         setTimeout(() => {
           this.loading = false;
         }, 800);
       });
+  },
+  methods: {
+    clickGeneri(generiToFilter) {
+      console.log(generiToFilter);
+      this.categoryFilter = generiToFilter;
+    },
   },
 };
 </script>
---
+++ src/components/FiltroGeneri.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <select
+      v-model="genre"
+      id="select-bar"
+      @change="$emit('badgeClick', genre)"
+    >
+      <option value="" selected>Tutti i generi</option>
+      <option :value="category" v-for="(category, i) in categoryList" :key="i">
+        {{ category }}
+      </option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FiltroGeneri",
+  data() {
+    return {
+      genre: "",
+    };
+  },
+  props: {
+    categoryList: Array,
+  },
+};
+</script>
+
+<style lang="sass">
+@import "@/styles/filtrogeneri"
+</style>
